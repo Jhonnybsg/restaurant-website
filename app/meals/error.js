5cd32c5c571e8fa18f1e4ffd1023d01586330724
Jhonny B. <jhonnybsg.dev@gmail.com>
2025-04-16 22:05:25 -0300

    add error and not found state management

---
+++ app/meals/error.js
@@ -0,0 +1,7 @@
+'use client';
+export default function Error() {
+    return <main className="error">
+        <h1>An Error occurred!</h1>
+        <p>Failed o fetch meals. Please try again later!</p>
+    </main>
+}
--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -2,10 +2,18 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '../../components/meals/meals-grid'; // Uncomment and update the path if MealsGrid is a separate component
 import { getMeals } from '../../lib/meals';
+import { Suspense } from 'react';
+
+
+async function Meals() {
+  const meals = await getMeals();
+
+  //throw Error();
+
+  return <MealsGrid meals={meals}/>
+}
 
 export default async function MealsPage() {
-
-  const meals = await getMeals();
 
   return (<>
     <header className={classes.header}>
@@ -21,7 +29,9 @@ export default async function MealsPage() {
       </p>
     </header>
     <main className={classes.main}>
-      <MealsGrid meals={meals}/>
+      <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+        <Meals />
+      </Suspense>
     </main>
   </>)
 }
